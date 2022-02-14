324d7b6b4d2926fd71fdb921e060033dca74e8b1
Sean Morgan <scm0323@att.net>
2022-02-14 17:11:21 -0600

    Add project files.

---
+++ DiceRoll/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DiceRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RollDice sixSides = new RollDice(6);
+            RollDice twentySides = new RollDice(20);
+
+            Console.WriteLine(sixSides);
+
+            for(int i = 0; i < 20; i++)
+            {
+                Console.Write(sixSides.Roll() + " ");
+            }
+
+            Console.WriteLine("\n\n" + twentySides);
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(twentySides.Roll() + " ");
+            }
+        }
+    }
+}
---
+++ DiceRoll/RollDice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DiceRoll
+{
+    internal class RollDice
+    {
+        private Random random;
+        private int sidesCount;
+        public RollDice(int sidesCount)
+        {
+            random = new Random();
+            this.sidesCount = sidesCount;
+        }
+
+        public int Roll()
+        {
+            return random.Next(1, sidesCount + 1);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Rolling a die with {0} sides", sidesCount);
+        }
+    }
+}
